0700695d7e2d65e1fef32bbf4eac24f1cefd71c4
Yuuki Wesp <ls-micro@ya.ru>
2019-05-10 14:23:04 +0300

    add record db model

---
+++ lib/Core/db/models/Record.cs
@@ -0,0 +1,15 @@
+﻿namespace XSing.Core.db.models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using DNS.Protocol;
+
+    public class Record
+    {
+        [Key]
+        public Guid UID { get; set; }
+        public RecordType Type { get; set; }
+        public string Domain { get; set; }
+        public string Value { get; set; }
+    }
+}
