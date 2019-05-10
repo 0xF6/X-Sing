9fdf4cba9942e8f61c7eb7d7ced4a79c4399731e
Yuuki Wesp <ls-micro@ya.ru>
2019-05-10 14:23:19 +0300

    add db context

---
+++ lib/Core/db/SingContext.cs
@@ -0,0 +1,19 @@
+﻿namespace XSing.Core.db
+{
+    using env;
+    using etc;
+    using Microsoft.EntityFrameworkCore;
+    using models;
+
+    public class SingContext : DbContext
+    {
+        public DbSet<Record> Records { get; set; }
+        public DbSet<Setting> Settings { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source={Env.ContentPath.WithCombine("xsing.db")}");
+        }
+    }
+}
