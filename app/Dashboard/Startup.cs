be1b1a74296cde1e089b23efd9360ccf6f99821a
Yuuki Wesp <ls-micro@ya.ru>
2019-05-10 05:02:28 +0300

    add dashboard files

---
+++ app/Dashboard/Startup.cs
@@ -0,0 +1,15 @@
+﻿namespace XSign
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.AspNetCore.Rewrite.Internal.UrlMatches;
+  
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
