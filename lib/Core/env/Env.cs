ad054e9d028466d0ffb3757431c43cc9ec92218f
Yuuki Wesp <ls-micro@ya.ru>
2019-05-10 14:23:25 +0300

    add env class

---
+++ lib/Core/env/Env.cs
@@ -0,0 +1,20 @@
+﻿namespace XSing.Core.env
+{
+    using System;
+    using System.IO;
+
+    public static class Env
+    {
+        public static string ContentPath
+        {
+            get
+            {
+                var path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                path = Path.Combine(path, $@"xsign\{Version}");
+                Directory.CreateDirectory(path);
+                return path;
+            }
+        }
+        public static string Version => "0.14";
+    }
+}
---
+++ lib/Core/etc/PathEx.cs
@@ -0,0 +1,9 @@
+﻿namespace XSing.Core.etc
+{
+    using System.IO;
+
+    public static class PathEx
+    {
+        public static string WithCombine(this string path1, string path2) => Path.Combine(path1, path2);
+    }
+}
