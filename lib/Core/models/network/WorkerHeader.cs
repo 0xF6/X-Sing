1f3e621b39137e35ec9513ce2a2bf8ca021629b7
Yuuki Wesp <ls-micro@ya.ru>
2019-05-14 13:38:19 +0300

    add worker header

---
+++ lib/Core/models/network/WorkerHeader.cs
@@ -0,0 +1,30 @@
+﻿namespace XSing.Core.models.network
+{
+    using System;
+    using System.Reflection;
+    using System.Runtime.Versioning;
+    using env;
+
+    public struct WorkerHeader
+    {
+        public Guid InstanceUID { get; set; }
+        public string Version { get; set; }
+        public string RuntimeVersion { get; set; }
+        public OperatingSystem OSInfo { get; set; }
+
+        public static string GetRuntimeVersion() => Assembly
+            .GetEntryAssembly()?
+            .GetCustomAttribute<TargetFrameworkAttribute>()?
+            .FrameworkName;
+        public static WorkerHeader Collect(Guid instanceUID)
+        {
+            return new WorkerHeader
+            {
+                InstanceUID = instanceUID,
+                OSInfo = Environment.OSVersion,
+                RuntimeVersion = GetRuntimeVersion(),
+                Version = Env.Version
+            };
+        }
+    }
+}
