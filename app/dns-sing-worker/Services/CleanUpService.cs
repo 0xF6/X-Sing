10e39f4c6880a948d5c0b69967cbe33468cc0df4
Yuuki Wesp <ls-micro@ya.ru>
2019-05-14 13:38:30 +0300

    add clean up service to worker

---
+++ app/dns-sing-worker/Services/CleanUpService.cs
@@ -0,0 +1,29 @@
+﻿namespace XSign.Worker.Services
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Hosting;
+    using XSing.Core.etc;
+
+    public class CleanUpService : BackgroundService
+    {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            "error.dump.log".AsFile().When(x => x.Exists, x => x.Delete());
+
+            return Task.CompletedTask;
+        }
+    }
+
+    public static class Sd
+    {
+        public static T When<T>(this T t, Func<T, bool> condition, Action<T> actor)
+        {
+            if (condition(t))
+                actor(t);
+            return t;
+        }
+    }
+
+}
